471d2da9ebcb88283aeb2f1a51d6291ee20af20b
ALLUK <lam83@bk.ru>
2021-12-11 23:24:07 +0500

    save master 11/12/21

--- Program.cs
+++ Program.cs
@@ -12,7 +12,21 @@ namespace WinCommander
         static void Main(string[] args)
         {
 
+            string workDir = @"C:\ExampleDir";
+            
+            // Перечень всех файлов и папок, вложенных в workDir
+            string[] entries = Directory.GetFileSystemEntries(workDir, "*", SearchOption.TopDirectoryOnly);
+            for (int i = 0; i < entries.Length; i++)
+            {
+                Console.WriteLine(entries[i]);
+            }
+
+
             Console.ReadLine();
         }
+
     }
 }
+//Просмотр файловой структуры;
+//Вывод дерева файловой системы с условием "пейджинга" - только 2 уровня;
+//Перемещение по папкам с помощью стрелок.
